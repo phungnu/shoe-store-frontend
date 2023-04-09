e4e0f69deda124d1d317f4973c19291c7b7db011
manh <manhdd162@gmail.com>
2023-04-10 06:46:37 +0700

    update call open api get location

--- src/conponents/screen/cart/index.js
+++ src/conponents/screen/cart/index.js
@@ -1,11 +1,13 @@
 import Header from "../../util/Header";
 import ProductInCart from "../../util/ProductInCart";
-import { Row, Col, Button, Modal, Input, Dropdown, Space, message } from "antd";
+import { Row, Col, Button, Modal, Input, Select } from "antd";
 import { DownOutlined, UserOutlined } from "@ant-design/icons";
 
 import "./style.css";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+
+import axios from "axios";
 
 const arrayShoes = [
   {
@@ -122,157 +124,198 @@ const arrayShoes = [
   },
 ];
 
-const items = [
-  {
-    label: "Lai Châu",
-    key: "1",
-    icon: <UserOutlined />,
-  },
-  {
-    label: "Hà Nội",
-    key: "2",
-    icon: <UserOutlined />,
-  },
-  {
-    label: "Sơn La",
-    key: "3",
-    icon: <UserOutlined />,
-  },
-  {
-    label: "Lào Cai",
-    key: "4",
-    icon: <UserOutlined />,
-  },
-];
-
 const Cart = () => {
-  const [total, setTotal] = useState(0);
+	const [total, setTotal] = useState(0);
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const showModal = () => {
-    setIsModalOpen(true);
-  };
-  const handleOk = () => {
-    setIsModalOpen(false);
-  };
-  const handleCancel = () => {
-    setIsModalOpen(false);
-  };
+	const showModal = () => {
+		setIsModalOpen(true);
+		callAPI(host);
+	};
+	const handleOk = () => {
+		setIsModalOpen(false);
+	};
+	const handleCancel = () => {
+		setIsModalOpen(false);
+	};
 
-  const sum = () => {
-    let tmp = 0;
-    for (let i = 0; i < arrayShoes.length; i++) {
-      tmp += arrayShoes[i].price * arrayShoes[i].amount;
-    }
-    setTotal(tmp);
-  };
-  useEffect(() => {
-    sum();
-  }, []);
-  const navigate = useNavigate();
-  const handleBack = () => {
-    navigate("/");
-  };
+	const sum = () => {
+		let tmp = 0;
+		for (let i = 0; i < arrayShoes.length; i++) {
+		tmp += arrayShoes[i].price * arrayShoes[i].amount;
+		}
+		setTotal(tmp);
+	};
+	useEffect(() => {
+		sum();
+	}, []);
+	const navigate = useNavigate();
+	const handleBack = () => {
+		navigate("/");
+	};
 
-  const handleMenuClick = (e) => {
-    message.info("Click on menu item.");
-    console.log("click", e);
-  };
+	const [cityList, setCityList] = useState([]);
+	const [districtList, setDistrictList] = useState([]);
+	const [wardList, setWardList] = useState([]);
 
-  const menuProps = {
-    items,
-    onClick: handleMenuClick,
-  };
+	const [city, setCity] = useState('');
+	const [district, setDistrict] = useState('');
+	const [ward, setWard] = useState('');
 
-  return (
-    <div className="pageCart">
-      <Header page={"cart"} />
-      <Row className="content">
-        <Col className="product-name" span={7}>
-          Product Name
-        </Col>
-        <Col className="quantity">Quantity</Col>
-        <Col className="subtotal">Subtotal</Col>
-        <Col className="total">Total</Col>
-        <Col className="clear-cart">Clear</Col>
-      </Row>
-      {arrayShoes.map((shoe) => (
-        <ProductInCart
-          name={shoe.name}
-          url={shoe.url}
-          price={shoe.price}
-          quantity={shoe.quantity}
-          amount={shoe.amount}
-          changeTotal={setTotal}
-        />
-      ))}
+	const host = "https://provinces.open-api.vn/api/";
+	
+	var callAPI = (api) => {
+		return axios.get(api)
+			.then((response) => {
+				renderData(response.data, "province");
+			});
+	}
 
-      <Row className="bottom">
-        <Button className="bottom-back" onClick={handleBack}>
-          Back to Shopping
-        </Button>
-        <Col className="buy-total">
-          <p className="total-money">Total: ${total}</p>
-          <Button onClick={showModal} className="bottom-buy">
-            Buy
-          </Button>
-        </Col>
-      </Row>
+	var callApiDistrict = (api) => {
+		return axios.get(api)
+			.then((response) => {
+				renderData(response.data.districts, "district");
+			});
+	}
+	var callApiWard = (api) => {
+		return axios.get(api)
+			.then((response) => {
+				renderData(response.data.wards, "ward");
+			});
+	}
+	
+	var renderData = (array, select) => {
+		const tmp = [];
+		array.forEach(element => {
+			tmp.push({
+				label : element.name,
+				value: element.code
+			})
+		});
+		if ( select=='province' ) {
+			setCityList(tmp);
+		} else if ( select == 'district' ) {
+			setDistrictList(tmp);
+		} else {
+			setWardList(tmp);
+		}
+	}
 
-      <Modal
-        title="Thông tin người mua"
-        open={isModalOpen}
-        onOk={handleOk}
-        onCancel={handleCancel}
-		width={650}
-      >
-        <div>
-          <p>Họ và tên<span className="obligatory">*</span></p>
-          <Row className="name-custmmer">
-            <Input className="first-name" placeholder="Họ" />
-            <Input className="last-name" placeholder="Tên" />
-          </Row>
-          <p>Email<span className="obligatory">*</span></p>
-          <Row>
-            <Input className="email" placeholder="Email" />
-          </Row>
-          <p>Số điện thoại<span className="obligatory">*</span></p>
-          <Row>
-            <Input className="phone" placeholder="Số điện thoại" />
-          </Row>
-          <p>Địa chỉ<span className="obligatory">*</span></p>
-          <Dropdown menu={menuProps}>
-            <Button>
-              <Space>
-                --Chọn Tỉnh/ Thành Phố--
-                <DownOutlined />
-              </Space>
-            </Button>
-          </Dropdown>
-		  <Dropdown menu={menuProps}>
-            <Button>
-              <Space>
-                --Chọn Huyện/ Quận--
-                <DownOutlined />
-              </Space>
-            </Button>
-          </Dropdown>
-		  <Dropdown menu={menuProps}>
-            <Button>
-              <Space>
-                --Chọn Phường/ Xã--
-                <DownOutlined />
-              </Space>
-            </Button>
-          </Dropdown>
-		  <Input className="apartment-number" placeholder="Số nhà" />
-		  <p>Lời nhắn</p>
-		  <Input className="message" />
-        </div>
-      </Modal>
-    </div>
-  );
+	const handleChooseCity = (option) => {
+		setCity(option.label);
+		callAPI('https://provinces.open-api.vn/api/?depth=1');
+		callApiDistrict(host + "p/" + option.value + "?depth=2");
+	}
+
+	const handleChooseDistrict = (option) => {
+		setDistrict(option.label);
+		callApiWard(host + "d/" + option.value + "?depth=2");
+	}
+
+	const handleChooseWard = (option) => {
+		setWard(option.label);
+	}
+
+	return (
+		<div className="pageCart">
+			<Header page={"cart"} />
+			<Row className="content">
+				<Col className="product-name" span={7}>
+				Product Name
+				</Col>
+				<Col className="quantity">Quantity</Col>
+				<Col className="subtotal">Subtotal</Col>
+				<Col className="total">Total</Col>
+				<Col className="clear-cart">Clear</Col>
+			</Row>
+			{arrayShoes.map((shoe) => (
+				<ProductInCart
+					name={shoe.name}
+					url={shoe.url}
+					price={shoe.price}
+					quantity={shoe.quantity}
+					amount={shoe.amount}
+					changeTotal={setTotal}
+				/>
+			))}
+
+			<Row className="bottom">
+				<Button className="bottom-back" onClick={handleBack}>
+					Back to Shopping
+				</Button>
+				<Col className="buy-total">
+				<p className="total-money">Total: ${total}</p>
+				<Button onClick={showModal} className="bottom-buy">
+					Buy
+				</Button>
+				</Col>
+			</Row>
+
+			<Modal
+				title="Thông tin người mua"
+				open={isModalOpen}
+				onOk={handleOk}
+				onCancel={handleCancel}
+				width={650}
+			>
+			<div>
+				<p>
+					Họ và tên<span className="obligatory">*</span>
+				</p>
+				<Row className="name-custmmer">
+					<Input className="first-name" placeholder="Họ" />
+					<Input className="last-name" placeholder="Tên" />
+				</Row>
+				<p>
+					Email<span className="obligatory">*</span>
+				</p>
+				<Row>
+					<Input className="email" placeholder="Email" />
+				</Row>
+				<p>
+					Số điện thoại<span className="obligatory">*</span>
+				</p>
+				<Row>
+					<Input className="phone" placeholder="Số điện thoại" />
+				</Row>
+				<p>
+					Địa chỉ<span className="obligatory">*</span>
+				</p>
+				<Select
+					labelInValue
+					defaultValue="--Chọn thành phố--"
+					style={{
+					width: 200,
+					}}
+					onChange={handleChooseCity}
+					options={cityList}
+				/>
+				<Select
+					labelInValue
+					defaultValue="--Chọn quận/huyện--"
+					style={{
+					width: 200,
+					}}
+					onChange={handleChooseDistrict}
+					options={districtList}
+				/>
+				<Select
+					labelInValue
+					defaultValue="--Chọn thị trấn/xã--"
+					style={{
+					width: 200,
+					}}
+					onChange={handleChooseWard}
+					options={wardList}
+				/>
+				<Input className="apartment-number" placeholder="Số nhà" />
+				<p>Lời nhắn</p>
+				<Input className="message" />
+			</div>
+		</Modal>
+		</div>
+	);
 };
 
 export default Cart;
