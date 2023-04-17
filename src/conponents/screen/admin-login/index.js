bb2133c9a2b4d09e88b42b42728f4ace9aec729c
phungnu <phungnu15032002@gmail.com>
2023-04-17 09:52:24 +0700

    update

--- src/conponents/screen/admin-login/index.js
+++ src/conponents/screen/admin-login/index.js
@@ -3,51 +3,96 @@ import { LockOutlined, UserOutlined } from "@ant-design/icons";
 import { Button, Form, Input } from "antd";
 import "./style.css";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import {toast} from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+
+
 const Admin = () => {
   
-  const navigate = useNavigate();
-  const onFinish = (values) => {
-    console.log("Received values of form: ", values);
-    navigate('/admin/manage');
-  };
+	const navigate = useNavigate();
 
-  return (
-    <div className="content-admin-login">
-      <Form name="normal_login" onFinish={onFinish} className="form">
-	  	<p className="label-admin-login">Admin Login</p>
-        <Form.Item
-          style={{width:'22%'}}
-          name="username"
-          rules={[{ required: true, message: "Please input your Username!" }]}
-        >
-          <Input
-            prefix={<UserOutlined className="site-form-item-icon" />}
-            placeholder="Username"
-          />
-        </Form.Item>
-        <Form.Item
-        style={{width:'22%'}}
-          name="password"
-          rules={[{ required: true, message: "Please input your Password!" }]}
-        >
-          <Input
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Item>
-        <Form.Item>
-          <Button
-            type="primary"
-            htmlType="submit"
-            className="login-form-button"
-          >
-            Log in
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
-  );
+	toast.configure();
+	
+	const  checkLogin = async (values) => {
+		console.log("Received values of form: ", values);
+		let response;
+        let code = 222;
+		await axios.post(`http://localhost:3008/users/login`, {
+			username: values.username,
+			password: values.password
+		}).then( res => {
+				console.log(res);
+				if ( res.data.statusCode=='OK') {
+					response = res.data;
+					code = 200
+				}
+			})
+			.catch(error => {
+				code = 404
+			});
+		if (code == 200 ) {
+			notify('success');
+			setTimeout(() => {
+				navigate('/admin/manage');
+			}, 1000)
+		} else {
+			notify("fail");
+		}
+		return response;		
+			
+	}
+
+	const notify = (info) => {
+        switch(info) {
+            case 'success':
+                toast.success('Login success');
+                break;
+            case 'fail' :
+                toast.error('username or password incorrect');
+                break;
+            default:
+                
+        }
+    }
+
+	return (
+		<div className="content-admin-login">
+			<Form name="normal_login" onFinish={checkLogin} className="form">
+				<p className="label-admin-login">Admin Login</p>
+				<Form.Item
+					style={{width:'22%'}}
+					name="username"
+					rules={[{ required: true, message: "Please input your Username!" }]}
+				>
+					<Input
+						prefix={<UserOutlined className="site-form-item-icon" />}
+						placeholder="Username"
+					/>
+				</Form.Item>
+				<Form.Item
+					style={{width:'22%'}}
+					name="password"
+					rules={[{ required: true, message: "Please input your Password!" }]}
+				>
+					<Input
+						prefix={<LockOutlined className="site-form-item-icon" />}
+						type="password"
+						placeholder="Password"
+					/>
+				</Form.Item>
+				<Form.Item>
+					<Button
+						type="primary"
+						htmlType="submit"
+						className="login-form-button"
+					>
+						Log in
+					</Button>
+				</Form.Item>
+			</Form>
+		</div>
+	);
 };
 
 export default Admin;
