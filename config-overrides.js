948a40bc65bbb415b2646f786f36b2f2b76075e6
manh610 <madridista6102000@gmail.com>
2024-06-03 15:44:54 +0700

    export template docx

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 
 module.exports = function override(config) {
+  // Thêm fallback cho các module cần thiết
   config.resolve.fallback = {
     ...config.resolve.fallback,
     util: require.resolve('util/'),
@@ -8,13 +9,20 @@ module.exports = function override(config) {
     crypto: require.resolve('crypto-browserify'),
     stream: require.resolve('stream-browserify'),
     path: require.resolve('path-browserify'),
-    fs: false, // fs không thể sử dụng trong trình duyệt, nên cần bỏ qua
+    http: require.resolve('stream-http'),
+    https: require.resolve('https-browserify'),
+    zlib: require.resolve('browserify-zlib'),
+    assert: require.resolve('assert/'), // Thêm polyfill cho assert
+    fs: false
   };
+
+  // Thêm các plugin cần thiết
   config.plugins = (config.plugins || []).concat([
     new webpack.ProvidePlugin({
       process: 'process/browser',
       Buffer: ['buffer', 'Buffer'],
     }),
   ]);
+
   return config;
 };
--- src/conponents/screen/home/index.js
+++ src/conponents/screen/home/index.js
@@ -15,14 +15,13 @@ import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/pagination';
 
-import PizZip from 'pizzip';
-import Docxtemplater from 'docxtemplater';
-import { saveAs } from 'file-saver';
+import { saveAs } from "file-saver";
 
 import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
 
+import { Document, Packer, Paragraph, TextRun, Table, TableRow, TableCell, WidthType, AlignmentType, BorderStyle } from 'docx';
 
-const Home = ({searchText, shoeContainerRef}) => {
+const Home = ({ searchText, shoeContainerRef }) => {
 	const [arrayShoe, setArrayShoes] = useState([]);
 	const [arrayShoeConstant, setArrayShoesconstant] = useState([]);
 
@@ -50,75 +49,303 @@ const Home = ({searchText, shoeContainerRef}) => {
 
 	useEffect(() => {
 		console.log(searchText);
-		if ( !searchText )
+		if (!searchText)
 			return;
 		var newArray = [];
-		for ( var i in arrayShoeConstant){
-			if (arrayShoeConstant[i].name.toLowerCase().includes(searchText.toLowerCase())){
+		for (var i in arrayShoeConstant) {
+			if (arrayShoeConstant[i].name.toLowerCase().includes(searchText.toLowerCase())) {
 				newArray.push(arrayShoeConstant[i]);
 			}
 		}
 		setArrayShoes(newArray);
-		shoeContainerRef.current?.scrollIntoView({ behavior: 'smooth'});
+		shoeContainerRef.current?.scrollIntoView({ behavior: 'smooth' });
 	}, [searchText])
 
 	useEffect(() => {
 		getDataListShoe()
 	}, [])
 
-	const contentRef = useRef();
-
 	const onExport = async () => {
-		const content = contentRef.current.innerHTML;
-	
-		const response = await fetch('/template.docx');
-		console.log(response);
-		if (!response.ok) {
-			throw new Error('Network response was not ok');
-		}
-		const arrayBuffer = await response.arrayBuffer();
-		const zip = new PizZip(arrayBuffer);
-	
-		const doc = new Docxtemplater(zip, {
-			paragraphLoop: true,
-			linebreaks: true,
+		const doc = new Document({
+			sections: [
+				{
+					properties: {},
+					children: [
+						new Paragraph({
+							children: [
+								new TextRun({
+									text: "CỘNG HÒA XÃ HỘI CHỦ NGHĨA VIỆT NAM",
+									size: 28,
+									break: 1,
+								}),
+								new TextRun({
+									text: "Độc lập – Tự do – Hạnh phúc",
+									size: 28,
+									break: 1,
+								}),
+							],
+							alignment: AlignmentType.CENTER,
+						}),
+						new Paragraph({
+							children: [
+								new TextRun({
+									text: "Hà Nội, ngày ... tháng ... năm ...",
+									size: 28,
+									break: 1,
+								}),
+							],
+							alignment: AlignmentType.RIGHT,
+						}),
+						new Paragraph({
+							children: [
+								new TextRun({
+									text: "BÁO CÁO DOANH THU KẾT QUẢ HOẠT ĐỘNG KINH DOANH",
+									bold: true,
+									size: 30,
+									break: 2,
+								}),
+								new TextRun({
+									text: "Tháng ...",
+									bold: true,
+									size: 28,
+									break: 1,
+								}),
+							],
+							alignment: AlignmentType.CENTER,
+						}),
+						new Paragraph({
+							children: [
+								new TextRun({
+									text: "Đơn vị báo cáo: Myshoes",
+									size: 28,
+									break: 2,
+								}),
+								new TextRun({
+									text: "Người lập báo cáo: ...",
+									size: 28,
+									break: 1,
+								}),
+								new TextRun({
+									text: "1. Tổng doanh thu: ...",
+									size: 28,
+									break: 1,
+								}),
+								new TextRun({
+									text: "2. Lợi nhuận thu được: ...",
+									size: 28,
+									break: 1,
+								}),
+								new TextRun({
+									text: "3. Tổng số đơn hàng bán ra: ...",
+									size: 28,
+									break: 1,
+								}),
+								new TextRun({
+									text: "4. Tỉ lệ đơn hàng thành công: ...",
+									size: 28,
+									break: 1,
+								}),
+								new TextRun({
+									text: "5. Tỉ lệ khách hàng truy cập thành khách hàng tiềm năng: ...",
+									size: 28,
+									break: 1,
+								}),
+								new TextRun({
+									text: "6. Doanh thu tháng trước: ...",
+									size: 28,
+									break: 1,
+								}),
+								new TextRun({
+									text: "7. Doanh thu tháng này: ...",
+									size: 28,
+									break: 1,
+								}),
+								new TextRun({
+									text: "8. Tháng có doanh thu cao nhất: ...",
+									size: 28,
+									break: 1,
+								}),
+								new TextRun({
+									text: "9. Mức độ tăng trưởng doanh thu so với tháng trước: ...",
+									size: 28,
+									break: 1,
+								}),
+								new TextRun({
+									text: "10. Top mặt hàng được bán chạy",
+									size: 28,
+									break: 1,
+								}),
+								new TextRun({
+									text: "    a. Top 1: ... 	Số lượt bán: ...",
+									size: 28,
+									break: 1,
+								}),
+								new TextRun({
+									text: "    b. Top 2: ...	Số lượt bán: ...",
+									size: 28,
+									break: 1,
+								}),
+								new TextRun({
+									text: "    c. Top 3: ...	Số lượt bán: ...",
+									size: 28,
+									break: 1,
+								}),
+								new TextRun({
+									text: "11.  Thành phố có doanh thu cao nhất: ...",
+									size: 28,
+									break: 1,
+								}),
+							],
+							alignment: AlignmentType.LEFT,
+							spacing: {
+								line: 400
+							},
+						}),
+						new Paragraph({
+							children: [
+								new TextRun({
+									text: "",
+									break: 2, // Thêm 2 dòng trống
+								}),
+							],
+						}),
+						new Table({
+							rows: [
+								new TableRow({
+									children: [
+										new TableCell({
+											children: [
+												new Paragraph({
+													children: [
+														new TextRun({
+															text: "Quản lý cửa hàng",
+															bold: true,
+															size: 28,
+														}),
+													],
+													alignment: AlignmentType.LEFT,
+												}),
+											],
+											borders: {
+												top: { style: BorderStyle.NONE, size: 0, color: "FFFFFF" },
+												bottom: { style: BorderStyle.NONE, size: 0, color: "FFFFFF" },
+												left: { style: BorderStyle.NONE, size: 0, color: "FFFFFF" },
+												right: { style: BorderStyle.NONE, size: 0, color: "FFFFFF" },
+											},
+											width: {
+												size: 50,
+												type: WidthType.PERCENTAGE,
+											},
+										}),
+										new TableCell({
+											children: [
+												new Paragraph({
+													children: [
+														new TextRun({
+															text: "Người báo cáo",
+															bold: true,
+															size: 28,
+														}),
+													],
+													alignment: AlignmentType.RIGHT,
+												}),
+											],
+											borders: {
+												top: { style: BorderStyle.NONE, size: 0, color: "FFFFFF" },
+												bottom: { style: BorderStyle.NONE, size: 0, color: "FFFFFF" },
+												left: { style: BorderStyle.NONE, size: 0, color: "FFFFFF" },
+												right: { style: BorderStyle.NONE, size: 0, color: "FFFFFF" },
+											},
+											width: {
+												size: 50,
+												type: WidthType.PERCENTAGE,
+											},
+										}),
+									],
+								}),
+								new TableRow({
+									children: [
+										new TableCell({
+											children: [
+												new Paragraph({
+													children: [
+														new TextRun({
+															text: "(Ký và ghi rõ họ tên)",
+															size: 28,
+														}),
+													],
+													alignment: AlignmentType.LEFT,
+												}),
+											],
+											borders: {
+												top: { style: BorderStyle.NONE, size: 0, color: "FFFFFF" },
+												bottom: { style: BorderStyle.NONE, size: 0, color: "FFFFFF" },
+												left: { style: BorderStyle.NONE, size: 0, color: "FFFFFF" },
+												right: { style: BorderStyle.NONE, size: 0, color: "FFFFFF" },
+											},
+											width: {
+												size: 50,
+												type: WidthType.PERCENTAGE,
+											},
+										}),
+										new TableCell({
+											children: [
+												new Paragraph({
+													children: [
+														new TextRun({
+															text: "(Ký và ghi rõ họ tên)",
+															size: 28,
+														}),
+													],
+													alignment: AlignmentType.RIGHT,
+												}),
+											],
+											borders: {
+												top: { style: BorderStyle.NONE, size: 0, color: "FFFFFF" },
+												bottom: { style: BorderStyle.NONE, size: 0, color: "FFFFFF" },
+												left: { style: BorderStyle.NONE, size: 0, color: "FFFFFF" },
+												right: { style: BorderStyle.NONE, size: 0, color: "FFFFFF" },
+											},
+											width: {
+												size: 50,
+												type: WidthType.PERCENTAGE,
+											},
+										}),
+									],
+								}),
+							],
+							width: {
+								size: 100,
+								type: WidthType.PERCENTAGE,
+							},
+						}),
+					],
+				},
+			],
 		});
-	
-		doc.setData({ content });
-	
-		doc.render();
-	
-		const out = doc.getZip().generate({
-			type: 'blob',
-			mimeType: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+
+		Packer.toBlob(doc).then((blob) => {
+			saveAs(blob, "example.docx");
 		});
-	
-		saveAs(out, 'document123.docx');
 	};
 
 	return (
 		<div>
 			<div className="introduce-container">
 				<Button onClick={onExport}>Export</Button>
-				<div ref={contentRef} className="export-container">
-					<div className="export-header-container" style={{textAlign: 'center'}}>
-						<h2>CỘNG HÒA XÃ HỘI CHỦ NGHĨA VIỆT NAM</h2>
-						<h4>Độc lập - Tự do - Hạnh phúc</h4>
-					</div>
-				</div>
 				<div className="intro-image-ctn">
 					<Row>
-						<Col xs={24} sm={12} md={6} lg={6} xl={6}>
+						<Col xs={28} sm={12} md={6} lg={6} xl={6}>
 							<div className="intro-image">
 								<Image src="image/intro1.png" preview={false} />
 							</div>
 						</Col>
-						<Col xs={24} sm={12} md={12} lg={12} xl={12}>
+						<Col xs={28} sm={12} md={12} lg={12} xl={12}>
 							<div className="intro-image">
 								<Image src="image/intro2.png" preview={false} />
 							</div>
 						</Col>
-						<Col xs={24} sm={12} md={6} lg={6} xl={6}>
+						<Col xs={28} sm={12} md={6} lg={6} xl={6}>
 							<div className="intro-image">
 								<Image src="image/intro3.jpg" preview={false} />
 							</div>
@@ -127,22 +354,22 @@ const Home = ({searchText, shoeContainerRef}) => {
 				</div>
 				<div className="intro-info">
 					<Row>
-						<Col className="col-intro-info" xs={24} sm={8} md={8} lg={8} xl={8}>
-							<Image className="intro-icon" src="/image/verified.png" preview={false}/>
+						<Col className="col-intro-info" xs={28} sm={8} md={8} lg={8} xl={8}>
+							<Image className="intro-icon" src="/image/verified.png" preview={false} />
 							<div>Hàng chính hãng 100%</div>
 						</Col>
-						<Col className="col-intro-info" xs={24} sm={8} md={8} lg={8} xl={8}>
-							<Image className="intro-icon" src="/image/shipping.png" preview={false}/>
+						<Col className="col-intro-info" xs={28} sm={8} md={8} lg={8} xl={8}>
+							<Image className="intro-icon" src="/image/shipping.png" preview={false} />
 							<div>Miễn phí giao hàng với đơn &gt; 500k</div>
 						</Col>
-						<Col className="col-intro-info" xs={24} sm={8} md={8} lg={8} xl={8}>
-							<Image className="intro-icon" src="/image/two-line.png" preview={false}/>
+						<Col className="col-intro-info" xs={28} sm={8} md={8} lg={8} xl={8}>
+							<Image className="intro-icon" src="/image/two-line.png" preview={false} />
 							<div>Đổi hàng 30 ngày, bảo hành 6 tháng</div>
 						</Col>
 					</Row>
 				</div>
 			</div>
-			<div className="title-wrapper"  ref={shoeContainerRef}>
+			<div className="title-wrapper" ref={shoeContainerRef}>
 				<div className="main-title">
 					SẢN PHẨM MỚI
 				</div>
@@ -151,7 +378,7 @@ const Home = ({searchText, shoeContainerRef}) => {
 			</div>
 			<Row className="content-home">
 				{arrayShoe.map((shoe) => (
-					<Col className="gutter-row" sm={24} md={12} lg={8} xl={6}>
+					<Col className="gutter-row" sm={28} md={12} lg={8} xl={6}>
 						<Shoes
 							id={shoe.id}
 							name={shoe.name}
@@ -182,31 +409,31 @@ const Home = ({searchText, shoeContainerRef}) => {
 			>
 				<SwiperSlide>
 					<div className="feedback-ctn">
-						<Image className="feedback-img" src="/image/feedback1.jpeg" preview={false}/>
+						<Image className="feedback-img" src="/image/feedback1.jpeg" preview={false} />
 						<div className="feedback-text">Tôi đã mua cho cả 2 vợ chồng giày của Myshoes.vn và thật sự nó vô cùng chất lượng. Hàng đảm bảo chính hãng 100% và chính sách bảo hành rất yên tâm ạ. Cảm ơn Myshoes.vn!</div>
 					</div>
 				</SwiperSlide>
 				<SwiperSlide>
 					<div className="feedback-ctn">
-						<Image className="feedback-img" src="/image/feedback2.jpg" preview={false}/>
+						<Image className="feedback-img" src="/image/feedback2.jpg" preview={false} />
 						<div className="feedback-text">Myshoes.vn bán hàng chính hãng, giá rất ok, tôi đã mua một đôi giày chạy bộ của Nike đi rất êm và thích.</div>
 					</div>
 				</SwiperSlide>
 				<SwiperSlide>
 					<div className="feedback-ctn">
-						<Image className="feedback-img" src="/image/feedback3.jpg" preview={false}/>
+						<Image className="feedback-img" src="/image/feedback3.jpg" preview={false} />
 						<div className="feedback-text">Tìm một đôi giày ưng ý không hề dễ dàng, nhưng từ khi biết đến Myshoes.vn thì hoàn toàn tin tưởng, nhiều mẫu đẹp và đã chọn được một em Adidas ưng ý!</div>
 					</div>
 				</SwiperSlide>
 				<SwiperSlide>
 					<div className="feedback-ctn">
-						<Image className="feedback-img" src="/image/feedback4.jpg" preview={false}/>
+						<Image className="feedback-img" src="/image/feedback4.jpg" preview={false} />
 						<div className="feedback-text">Mới mua combo chăm sóc giày của Myshoes sử dụng rất tốt, vệ sinh giày siêu sạch, xịt nano rất hiệu quả khi đi trời mưa.</div>
 					</div>
 				</SwiperSlide>
 				<SwiperSlide>
 					<div className="feedback-ctn">
-						<Image className="feedback-img" src="/image/feedback5.jpg" preview={false}/>
+						<Image className="feedback-img" src="/image/feedback5.jpg" preview={false} />
 						<div className="feedback-text">Tôi đã mua cho cả 2 vợ chồng giày của Myshoes.vn và thật sự nó vô cùng chất lượng. Hàng đảm bảo chính hãng 100% và chính sách bảo hành rất yên tâm ạ. Cảm ơn Myshoes.vn!</div>
 					</div>
 				</SwiperSlide>
